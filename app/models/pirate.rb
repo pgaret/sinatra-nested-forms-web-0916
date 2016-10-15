080e4ff96dcdb542ca2f3961bb26fc63e3b30572
pgaret <peregringaret@gmail.com>
2016-10-15 11:20:33 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  @@all = []
+  attr_accessor :name, :height, :weight
+  def initialize(name = nil, height = nil, weight = nil)
+    @name = name
+    @height = height
+    @weight = weight
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def self.clear
+    @@all = []
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,16 @@
 class Ship
+  @@all = []
+  attr_accessor :name, :type, :booty
+  def initialize(name = nil, type = nil, booty = nil)
+    @name = name
+    @type = type
+    @booty = booty
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def self.clear
+    @@all = []
+  end
 end
